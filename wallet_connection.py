2bb3fc4f5da8941a785923c3b584d9dbfe008f4c
nikhil-itaims <nikhil.itaims@gmail.com>
2024-01-22 17:45:35 +0530

    connect wallet infura node

---
+++ wallet_connection.py
@@ -0,0 +1,14 @@
+from web3 import Web3, HTTPProvider
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+api_key = os.getenv('API_KEY')
+account_id = os.getenv('ACCOUNT_ID')
+
+w3 = Web3(HTTPProvider(f"https://mainnet.infura.io/v3/{api_key}"))
+
+if w3.is_connected():
+    balance = w3.eth.get_balance(account=account_id)
+    print(balance)
+    
